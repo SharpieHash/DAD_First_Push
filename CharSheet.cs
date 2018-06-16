1725a52ac71d284dc1fa98a89b1b93c9c1de0e50
Will Parker <tama3million@gmail.com>
2018-06-16 18:36:33 +0100

    Player and Equipment

---
+++ CharSheet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Character {
+    //Instance variables
+    private String plrName;
+    private String plrClass;
+    private String plrRace;
+    private int plrLevel;
+    private String plrBackground;
+    private String userName;
+    private String Alignment;
+    private int e
+
+    //Constructor
+	public Character() {
+
+	}
+}
